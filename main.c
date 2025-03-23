583cbb731fe239d8ddf872908fc57e73c445808a
feno-tolojanahry <fenoheriniainat@gmail.com>
2025-03-23 19:21:57 +0900

    memory safety

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define WORK_DIR "git"
+#define WORK_DIR "backup"
 
 void create_dir(char * dir_name) 
 {
@@ -22,6 +22,26 @@ int is_directory(const char * path)
     if (stat(path, &astatbuf) != 0) 
         return 0;
     return S_ISDIR(astatbuf.st_mode);
+}
+
+void * s_malloc(size_t size) 
+{
+    void *ptr = malloc(size);
+    if (ptr == NULL) {
+        printf("Error allocating memory");
+        exit(1);
+    }
+    return ptr;
+}
+
+void * s_realloc(void* ptr, size_t size)
+{
+    ptr = realloc(ptr, size);
+    if (ptr == NULL) {
+        printf("Error when reallocating memory");
+        exit(1);
+    }
+    return ptr;
 }
 
 int copy_content(const char * path, const char * dst_path) 
@@ -43,7 +63,7 @@ int copy_content(const char * path, const char * dst_path)
     }
 
     while ((ch = fgetc(subfile)) != EOF) {
-        printf("%c\n", ch);
+        // printf("%c\n", ch);
         fputc(ch, copyfile);
     }
     fclose(subfile);
@@ -53,7 +73,7 @@ int copy_content(const char * path, const char * dst_path)
 
 int cpy_file_path (char * filepath, char * relative_path_dr, char * filename) 
 {
-    char * copyfilepath = malloc(sizeof (char) * strlen(relative_path_dr));
+    char * copyfilepath = s_malloc(sizeof (char) * strlen(relative_path_dr) * strlen(filename) + 2);
 
     strcpy(copyfilepath, relative_path_dr);
     strcat(copyfilepath, "/");
@@ -78,61 +98,69 @@ int snapshot()
     }
 
     while((de = readdir(dr)) != NULL) {
-        char * subdir_path = malloc(sizeof (char) * strlen(path));
-        char * relative_path_subdr = malloc(sizeof (char) * strlen(WORK_DIR) + 1);
+        char * subdir_path = s_malloc(sizeof (char) * (strlen(path) + strlen(de->d_name) + 2));
+        char * relative_path_subdr = s_malloc(sizeof (char) * strlen(WORK_DIR) + 1);
 
         strcpy(subdir_path, path);
         strcat(subdir_path, "/");
         strcat(subdir_path, de->d_name);
         
         strcpy(relative_path_subdr, WORK_DIR);
-
-        if (strcmp(de->d_name, linuxDir) == 0 || strcmp(de->d_name, linuxDir2) == 0) 
+      
+        if (strcmp(de->d_name, linuxDir) == 0 || strcmp(de->d_name, linuxDir2) == 0) {
+            free(relative_path_subdr);
+            free(subdir_path);
             continue;
+        }
         if (is_directory(subdir_path) > 0) {
             DIR *subd = opendir(subdir_path);
             struct dirent *subde;
-
+            
+            relative_path_subdr = s_realloc(relative_path_subdr, sizeof (char) * (strlen(WORK_DIR) + strlen(de->d_name) + 2));
             strcat(relative_path_subdr, "/");
             strcat(relative_path_subdr, de->d_name);
             
             create_dir(relative_path_subdr);
             
             while ((subde = readdir(subd)) != NULL) {
-                char * filepath = malloc(sizeof (char) * (strlen(subdir_path) + strlen(subde->d_name)));
+                char * filepath = s_malloc(sizeof (char) * (strlen(subdir_path) + strlen(subde->d_name) + 2));
                 strcpy(filepath, subdir_path);
                 
                 strcat(filepath, "/");
                 strcat(filepath, subde->d_name);
 
-                if (strcmp(subde->d_name, linuxDir) == 0 || strcmp(subde->d_name, linuxDir2) == 0) 
+                if (strcmp(subde->d_name, linuxDir) == 0 || strcmp(subde->d_name, linuxDir2) == 0) {
+                    free(filepath);
                     continue;
+                }
                 // read content if a file
-                printf("\nfilepath: %s, relative_path_subdr: %s, sbde_name: %s", filepath, relative_path_subdr, subde->d_name);
                 if (is_directory(filepath) == 0) {
                     int res = cpy_file_path(filepath, relative_path_subdr, subde->d_name);
                     if (res == 1) {
                         printf("error copying file");
                     }
                 } else {
-                    printf("read directory content");
+                    // printf("read directory content");
                 }
+                free(filepath);
             }
+            closedir(subd);
         } else {
             printf("%s is not a directory", subdir_path);
 
             // if the content is a file make copie of file
-            char * org_filepath = malloc(sizeof (char) * strlen(subdir_path));
+            char * org_filepath = s_malloc(sizeof (char) * (strlen(subdir_path) + 1));
             strcpy(org_filepath, subdir_path);
             printf("\ncreate file %s, subdirpath: %s, filename: \"%s\" ", org_filepath, relative_path_subdr, de->d_name);
             int res = cpy_file_path(org_filepath, relative_path_subdr, de->d_name);
             if (res == 1) {
                 printf("error copying file");
             }
+            free(org_filepath);
         }
         free(relative_path_subdr);
         free(subdir_path);
-        printf("\n");
+    //     printf("\n");
     }
     closedir(dr);
     return 0;
---
+++ specification.md
@@ -0,0 +1,5 @@
+- duplicate folder and files
+- add a message in history when launching backup
+    - store information into database
+
+- get diff
